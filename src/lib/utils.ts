d4e636383d33bac22d5d1e1f313d8b13f5f59ad7
project <shaikhsaquibjamal@gmail.com>
2026-03-30 14:51:25 +0530

    fix missing convertFilesToTreeItems

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,13 @@
+export function convertFilesToTreeItems(files: any[]) {
+  return files.map((file) => ({
+    id: file.id || file.name,
+    name: file.name,
+    children: file.children
+      ? convertFilesToTreeItems(file.children)
+      : [],
+  }));
+}
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
